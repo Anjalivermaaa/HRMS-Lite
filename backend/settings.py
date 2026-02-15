bd99f3618e74bf164b8fbd3df2185f2c6284580d
Anjalivermaaa <vanjali842000@gmail.com>
2026-02-15 21:41:41 +0530

    Changes in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database Configuration
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
